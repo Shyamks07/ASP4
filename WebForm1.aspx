caafef4707f2f2e1f9bc6f8061b6de00ff7d841c
Shyamks07 <97223205+Shyamks07@users.noreply.github.com>
2024-08-12 09:39:08 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication5.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="padding:20px" >DAY&nbsp;
+            <asp:DropDownList ID="DropDownList1" runat="server"  ><asp:ListItem Value="">Select</asp:ListItem>  
+            </asp:DropDownList>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; MONTH&nbsp;
+            <asp:DropDownList ID="DropDownList2" runat="server"><asp:ListItem Value="">Select</asp:ListItem>
+            </asp:DropDownList>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; YEAR&nbsp;
+            <asp:DropDownList ID="DropDownList3" runat="server"><asp:ListItem Value="">Select</asp:ListItem>
+            </asp:DropDownList>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" /></div>
+       <div>
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+           <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="upload" />
+           <asp:Image ID="Image1" runat="server" />
+           <asp:ImageMap ID="ImageMap1" runat="server" OnClick="ImageMap1_Click" Width="100px" Height="100px">
+           </asp:ImageMap>
+           </div>
+        <asp:HyperLink ID="HyperLink1" runat="server" NavigateUrl="https://www.google.co.in">GOOGLE</asp:HyperLink>
+        <asp:LinkButton ID="LinkButton1" runat="server">LinkButton</asp:LinkButton>
+        </form>
+        </body>
+   
+    
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication5
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack != true)
+            {
+                show();
+            }
+        }
+
+
+        public void show()
+        {
+            for (int i = 1; i <= 31; i++)
+            {
+                DropDownList1.Items.Add(i.ToString());
+            }
+            for (int j = 1; j <= 12; j++)
+            {
+                DropDownList2.Items.Add(j.ToString());
+            }
+            for (int k = 1900; k <= 2100; k++)
+            {
+                DropDownList3.Items.Add(k.ToString());
+            }
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        protected void UploadFile(object sender, EventArgs e)
+        {
+            string folderPath = Server.MapPath("~/Files/");
+
+            //Check whether Directory (Folder) exists.
+            if (!Directory.Exists(folderPath))
+            {
+                //If Directory (Folder) does not exists Create it.
+                Directory.CreateDirectory(folderPath);
+            }
+
+            //Save the File to the Directory (Folder).
+            FileUpload1.SaveAs(folderPath + Path.GetFileName(FileUpload1.FileName));
+
+            //Display the Picture in Image control.
+            Image1.ImageUrl = "~/Files/" + Path.GetFileName(FileUpload1.FileName);
+        }
+
+        protected void ImageMap1_Click(object sender, ImageMapEventArgs e)
+        {
+            
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            string folderPath = Server.MapPath("~/Files/");
+
+            //Check whether Directory (Folder) exists.
+            if (!Directory.Exists(folderPath))
+            {
+                //If Directory (Folder) does not exists Create it.
+                Directory.CreateDirectory(folderPath);
+            }
+
+            //Save the File to the Directory (Folder).
+            FileUpload1.SaveAs(folderPath + Path.GetFileName(FileUpload1.FileName));
+
+            //Display the Picture in Image control.
+            
+            ImageMap1.ImageUrl = "~/Files/" + Path.GetFileName(FileUpload1.FileName);
+        }
+    }
+}
